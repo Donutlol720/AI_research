0347a604cf4676bbeda9cbcc5abf7a519bba579a
Donutlol720 <143463974+Donutlol720@users.noreply.github.com>
2025-07-23 22:51:03 -0500

    add 2024 12B for gemini pro 2.5

--- 3_analyze_results.py
+++ 3_analyze_results.py
@@ -32,7 +32,7 @@ def analyze_results(file_path):
 ## for all results
 if __name__ == "__main__":
 
-    filelist = [ '2022_12A','2022_12B']
+    filelist = [ '2024_12B']
     #filelist = [ '2022_12A','2022_12B','2023_12A','2023_12B', '2024_12A', '2024_12B']
 
     ##change here for different output file name
